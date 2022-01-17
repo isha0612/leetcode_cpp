945750f3ac6fecdcd6a540b5ca2419caf790a84b
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-01-17 20:03:06 +0530

    Create binary_tree_preorder_traversal.cpp

---
+++ binary_tree_preorder_traversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector <int> v;
+        if(root == NULL) return v;
+        solve(v, root);
+        return v;
+    }
+    void solve(vector <int> &v, TreeNode* root) {
+        v.push_back(root->val);
+        if(root->left) solve(v, root->left);
+        if(root->right) solve(v, root->right);
+    }
+};
+
