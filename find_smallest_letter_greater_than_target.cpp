9708206007a78ab8415716226ec8cd3496529342
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-03-27 22:41:41 +0530

    Create find_smallest_letter_greater_than_target.cpp

---
+++ find_smallest_letter_greater_than_target.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int i = 0, j = letters.size() - 1;
+        while(i <= j) {
+            int mid = (i + j) / 2;
+            if(letters[mid] > target) j = mid - 1;
+            else i = mid + 1;
+        }
+        return letters[i % letters.size()];
+    }
+};
