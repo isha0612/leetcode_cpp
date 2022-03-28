9e47f6fb1922503960a864a569da05b08b06bf73
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-03-28 23:50:11 +0530

    Create peak_index_in_a_mountain_array.cpp

---
+++ peak_index_in_a_mountain_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int i = 0, j = arr.size() - 1;
+        while(i < j) {
+            int mid = (i + j) / 2;
+            if(arr[mid] > arr[mid + 1]) {
+                j = mid;
+            }
+            else if(arr[mid] < arr[mid + 1]) {
+                i = mid + 1;
+            }
+        }
+        return i;
+    }
+};
