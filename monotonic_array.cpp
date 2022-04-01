158e5f0a17b1fa0d5cc66f0a5fc89d43e3ca56be
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-04-02 00:09:00 +0530

    Update monotonic_array.cpp

--- monotonic_array.cpp
+++ monotonic_array.cpp
@@ -12,3 +12,20 @@ public:
         return true;
     }
 };
+
+//another solution
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int i = 0, n = nums.size();
+        int dir = 0, ans = 0;
+        while(i < n - 1) {
+            if(nums[i] > nums[i + 1]) dir = -1;
+            else if(nums[i] < nums[i + 1]) dir = 1;
+            if(ans == 0) ans = dir;
+            else if (dir != ans) return false;
+            i++;
+        }
+        return true;
+    }
+};
