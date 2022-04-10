d1fc76ae09850deed38540794b6e1e9acbec0ae3
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-04-10 10:56:20 +0530

    Create crawler_log_folder.cpp

---
+++ crawler_log_folder.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        for(auto v : logs) {
+            if(v == "../") {
+                if(!st.empty()) st.pop();
+            }
+            else if(v == "./") {
+                continue;
+            }
+            else {
+                st.push(v);
+            }
+        }
+        int ans = 0;
+        while(!st.empty()) {
+            ans++;
+            st.pop();
+        }
+        return ans;
+    }
+};
