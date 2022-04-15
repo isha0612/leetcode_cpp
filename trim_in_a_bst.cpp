3e7dea47377188bc0b0deceffc52256b1d67406b
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-04-16 00:37:02 +0530

    Update trim_in_a_bst.cpp

--- trim_in_a_bst.cpp
+++ trim_in_a_bst.cpp
@@ -17,7 +17,7 @@ public:
         if(!(root->val >= low && root->val <= high))
         {
             if(root->val < low) root = trimBST(root->right, low, high);
-            else if(root->val > high)root = trimBST(root->left, low, high);
+            else if(root->val > high) root = trimBST(root->left, low, high);
             return root;
         }
         root->left = trimBST(root->left, low, high);
