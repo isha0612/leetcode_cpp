5af6a81a5d36fa547696eb97b3d40f75340a4e07
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-04-23 18:45:20 +0530

    Update shifting_letters.cpp

--- shifting_letters.cpp
+++ shifting_letters.cpp
@@ -13,3 +13,18 @@ public:
         return s;
     }
 };
+
+//another solution
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = shifts.size();
+        int sum = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            sum += shifts[i];
+            sum = sum % 26;
+            s[i] = (s[i] + sum - 'a') % 26 + 'a';
+        }
+        return s;
+    }
+};
