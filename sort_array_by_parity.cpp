54aa59f4e41dfe5d971c2a99cea018bea7dbe370
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-02 09:25:36 +0530

    Update sort_array_by_parity.cpp

--- sort_array_by_parity.cpp
+++ sort_array_by_parity.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = -1;
+        for(int k = 0; k < nums.size(); k++) {
+            if(nums[k] % 2 == 0) {
+                i++;
+                swap(nums[i], nums[k]);
+            }
+        }
+        return nums;
+    }
+};
+
+//another solution
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
         int n = nums.size();
         vector<int> res(n);
         int i = 0, j = n - 1;
