e21ee7a84438833d1cafc384d9c174031ff5790b
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-02 14:41:33 +0530

    Update remove_duplicates_from_sorted_list.cpp

--- remove_duplicates_from_sorted_list.cpp
+++ remove_duplicates_from_sorted_list.cpp
@@ -30,3 +30,31 @@ public:
         return head;
     }
 };
+
+//another solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode *temp = head;
+        while(temp && temp->next) {
+            while(temp && temp->next && (temp->val == temp->next->val)) {
+                ListNode *q = temp->next;
+                temp->next = temp->next->next;
+                delete q;
+            }
+            temp = temp->next;
+        }
+        return head;
+    }
+};
