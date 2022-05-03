10594bb845637bf0cb0e0fc24019326ee5278629
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-03 11:53:48 +0530

    Update last_stone_weight.cpp

--- last_stone_weight.cpp
+++ last_stone_weight.cpp
@@ -12,3 +12,22 @@ public:
         return stones[0];
     }
 };
+
+//another solution
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> q;
+        for(int i = 0; i < stones.size(); i++) {
+            q.push(stones[i]);
+        }
+        while(q.size() != 1) {
+            int val = q.top();
+            q.pop();
+            val -= q.top();
+            q.pop();
+            q.push(val);
+        }
+        return q.top();
+    }
+};
