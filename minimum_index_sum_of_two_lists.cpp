f70b4ba806ff86aadf33a718a29819c0d5adfc62
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-08 12:57:00 +0530

    Update minimum_index_sum_of_two_lists.cpp

--- minimum_index_sum_of_two_lists.cpp
+++ minimum_index_sum_of_two_lists.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2){
+        unordered_map<string, int> m;
+        int min = INT_MAX;
+        for(int i = 0; i < list1.size(); i++) {
+            m[list1[i]] = i + 1;
+        }
+        vector<string> res;
+        for(int i = 0; i < list2.size(); i++) {
+            if(m[list2[i]] > 0) {
+                m[list2[i]] += i;
+                if(m[list2[i]] < min) {
+                    res.clear();
+                    res.push_back(list2[i]);
+                }
+                else if(m[list2[i]] == min) {
+                    res.push_back(list2[i]);
+                }
+                min = min < m[list2[i]] ? min : m[list2[i]];
+            }
+        }
+        return res;
+    }
+};
+
+//another solution
+
+class Solution {
+public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         map<int, vector<string>> m;
         int min = INT_MAX;
