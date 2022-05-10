2afebdf0ffb874431bf7f4b4de39b4ae374d0f46
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-10 10:15:47 +0530

    Update combination_sum.cpp

--- combination_sum.cpp
+++ combination_sum.cpp
@@ -11,8 +11,10 @@ public:
     void backtrack(vector<int> &can, int idx, int target, vector<int> &res) {
         if(target < 0)
             return;
-        if(target == 0)
+        if(target == 0) {
             sol.push_back(res);
+            return;
+        }
         for(int i = idx; i < can.size(); i++) {
             res.push_back(can[i]);
             backtrack(can, i, target - can[i], res);
