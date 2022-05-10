0cad81e3d27ad0c0cc95a1abf9801da4106b72c2
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-10 09:34:46 +0530

    Create combination_sum.cpp

---
+++ combination_sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> sol;
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> res;
+        backtrack(candidates, 0, target, res);
+        return sol;
+    }
+    
+    void backtrack(vector<int> &can, int idx, int target, vector<int> &res) {
+        if(target < 0)
+            return;
+        if(target == 0)
+            sol.push_back(res);
+        for(int i = idx; i < can.size(); i++) {
+            res.push_back(can[i]);
+            backtrack(can, i, target - can[i], res);
+            res.pop_back();
+        }
+    }
+};
