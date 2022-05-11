5282b1420ab65bb7abad02a167adc373d5c40084
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-11 23:00:48 +0530

    Update intersection_of_two_arrays.cpp

--- intersection_of_two_arrays.cpp
+++ intersection_of_two_arrays.cpp
@@ -2,25 +2,6 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> sol;
-        for(int i = 0; i < nums1.size(); i++) {
-            for(int j = 0; j < nums2.size(); j++) {
-                if(nums1[i] == nums2[j]) {
-                    sol.push_back(nums1[i]);
-                    break;
-                }
-            }
-        }
-        sort(sol.begin(), sol.end());
-        sol.erase(unique(sol.begin(), sol.end()), sol.end());
-        return sol;
-    }
-};
-
-//another solution
-class Solution {
-public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> sol;
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
         int i = 0, j = 0;
