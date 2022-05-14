9d1a100d60fdd9fe5aaccc50aea89df8dc20145a
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-15 01:18:59 +0530

    Update power_of_three.cpp

--- power_of_three.cpp
+++ power_of_three.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        if(n <= 0) return false;
+        return pow(3, int(log2(n) / log2(3))) == n;
+    }
+};
+
+//another solution
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
         if(n == 0) return false;
         if(n % 3 == 0) return isPowerOfThree(n/3);
         if(n == 1) return true;
