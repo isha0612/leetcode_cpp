c59b2a0b584bc80cbb4c84551985f65d2a301d4b
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-14 22:38:12 +0530

    Update power_of_two.cpp

--- power_of_two.cpp
+++ power_of_two.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        return !(n & (n - 1));
+    }
+};
+
+//another solution
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
         if(n == 0) return false;
         return (ceil(log2(n)) == floor(log2(n)));
     }
