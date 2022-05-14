b882ea420bcea812617e2fd02fccf0c31fb1ccf4
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-15 00:35:43 +0530

    Update power_of_four.cpp

--- power_of_four.cpp
+++ power_of_four.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
+        if(n <= 0) return false;
+        int i = 0;
+        if(!(n & (n - 1))) {
+            while(n) {
+                n = n>>1;
+                i++;
+            }
+            if(i % 2 == 1) return true;
+            else return false;
+        }
+        return false;
+    }
+};
+
+//another solution
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
         if(n == 1) return true;
         if(n == 0) return false;
         if(n % 4 == 0) return isPowerOfFour(n/4);
