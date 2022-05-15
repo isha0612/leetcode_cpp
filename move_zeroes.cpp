3e086d8588e05f4c8ffa509067a5cbdd49abe628
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-16 00:40:07 +0530

    Update move_zeroes.cpp

--- move_zeroes.cpp
+++ move_zeroes.cpp
@@ -14,6 +14,7 @@ public:
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int i = -1, j = 0;
         while(j < nums.size()) {
             if(nums[j] != 0) {
                 i++;
