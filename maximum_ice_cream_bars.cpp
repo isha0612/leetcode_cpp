c74c1923f9acb357bb780e2b2fe2348f3a3850dc
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-15 23:57:19 +0530

    Create maximum_ice_cream_bars.cpp

---
+++ maximum_ice_cream_bars.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+        int ans = 0, sum = 0;
+        for(int i = 0; i < costs.size(); i++) {
+            sum += costs[i];
+            if(sum <= coins) ans++;
+            else break;
+        }
+        return ans;
+    }
+};
