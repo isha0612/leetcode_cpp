91ab4eb4f23935c370ea62fb23e56486ff5bdc6c
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-18 09:47:43 +0530

    Update single_number.cpp

--- single_number.cpp
+++ single_number.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int n = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            n = n ^ nums[i];
+        }
+        return n;
+    }
+};
+
+//another solution
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size() - 1; i++) {
             if(nums[i] != nums[i + 1]) return nums[i];
