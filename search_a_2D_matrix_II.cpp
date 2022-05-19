1c92bf4df7a45a0700def3c3a1f835170c9b5033
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-19 14:48:45 +0530

    Update search_a_2D_matrix_II.cpp

--- search_a_2D_matrix_II.cpp
+++ search_a_2D_matrix_II.cpp
@@ -17,3 +17,29 @@ public:
         return f;
     }
 };
+
+//another solution
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i = 0; i < matrix.size(); i++) {
+            if(binarySearch(matrix[i], target))
+                return true;
+        }
+        return false;
+    }
+    bool binarySearch(vector<int> &arr, int target) {
+        int i = 0, j = arr.size() - 1;
+        while(i <= j) {
+            int mid = i + (j - i) / 2;
+            if(arr[mid] == target) 
+               return true;
+            else if(arr[mid] < target) 
+               i = mid + 1;
+            else 
+               j = mid - 1;
+        }
+        return false;
+    }
+};
