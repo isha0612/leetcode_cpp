4b14c25f751d8c95beb3742f756d5457134f7e49
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-19 14:20:57 +0530

    Update sort_colors.cpp

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -2,6 +2,33 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
+        int zeroes = 0, twos = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 0) zeroes++;
+            else if(nums[i] == 2) twos++;
+        }
+        int i = 0, j = n - 1;
+        for(int c = 0; c < zeroes && i < n; c++) {
+            nums[i] = 0;
+            i++;
+        }
+        for(int c = 0; c < twos && j >= 0; c++) {
+            nums[j] = 2;
+            j--;
+        }
+        while(i <= j) {
+            nums[i] = nums[j] = 1;
+            i++; j--;
+        }
+    }
+};
+
+//another solution
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
         vector<int> ans(n);
         int i = 0, j = n - 1;
         for(int k = 0; k < n; k++) {
