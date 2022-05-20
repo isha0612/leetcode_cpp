d9204120e0f4dcaf297a0a1a3cf94763f0d29b3c
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-20 15:22:32 +0530

    Create unique_paths.cpp

---
+++ unique_paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        return find(m, n, dp);
+    }
+    int find(int m, int n, vector<vector<int>> &dp) {
+        if(n == 0 || m == 0) return 0;
+        if(n == 1 || m == 1) return 1;
+        if(dp[m][n] != -1) return dp[m][n];
+        int ans = find(m - 1, n, dp) + find(m, n - 1, dp);
+        return dp[m][n] = ans;
+    }
+};
