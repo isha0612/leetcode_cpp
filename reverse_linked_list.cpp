d37be3d5da73533f8a01f7ef5ea514604d7b5d8b
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-23 19:09:08 +0530

    Update reverse_linked_list.cpp

--- reverse_linked_list.cpp
+++ reverse_linked_list.cpp
@@ -26,3 +26,38 @@ public:
         return head;
     }
 };
+
+
+//another solution
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        stack<ListNode*> s;
+        while(head) {
+            s.push(head);
+            head = head->next;
+        }
+        head = s.top();
+        ListNode* temp = head;
+        s.pop();
+        while(!s.empty()) {
+            temp->next = s.top();
+            temp = temp->next;
+            s.pop();
+        }
+        temp->next = NULL;
+        return head;
+    }
+};
