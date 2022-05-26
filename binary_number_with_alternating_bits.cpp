e47ade9bd562a4dc9ca5517341705c6e64d36c53
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-26 21:55:44 +0530

    Update binary_number_with_alternating_bits.cpp

--- binary_number_with_alternating_bits.cpp
+++ binary_number_with_alternating_bits.cpp
@@ -13,3 +13,21 @@ public:
         return true;
     }
 };
+
+//another solution
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        vector<int> sol;
+        while(n) {
+            sol.push_back(n % 2);
+            n = n >> 1;
+        }
+        for(int i = 0; i < sol.size() - 1; i++) {
+            if(sol[i] == sol[i + 1])
+                return false;
+        }
+        return true;
+    }
+};
