e62ef5dc753effc7202aedee6584c603ec62856a
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-26 21:51:39 +0530

    Update number_of_1_bits.cpp

--- number_of_1_bits.cpp
+++ number_of_1_bits.cpp
@@ -10,3 +10,18 @@ public:
         return c;
     }
 };
+
+//another solution
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        uint32_t mask = (uint32_t)1;
+        int c = 0;
+        while(n) {
+            if(mask & n) c++;
+            n = n >> 1;
+        }
+        return c;
+    }
+};
