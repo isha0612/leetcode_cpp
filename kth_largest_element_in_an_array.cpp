b29e85fb06a90890d87126a321d283815cc79c7e
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-27 09:37:19 +0530

    Update kth_largest_element_in_an_array.cpp

--- kth_largest_element_in_an_array.cpp
+++ kth_largest_element_in_an_array.cpp
@@ -5,3 +5,18 @@ public:
         return nums[k - 1];
     }
 };
+
+//another solution
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue <int, vector<int>, greater<int>> pq;
+        for(int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
+            if(pq.size() > k)
+                pq.pop();
+        }
+        return pq.top();
+    }
+};
