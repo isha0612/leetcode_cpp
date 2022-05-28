d8ba5a7c5cee17d0276364f864479fb6314f43fa
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-28 23:30:07 +0530

    Update jewels_and_stones.cpp

--- jewels_and_stones.cpp
+++ jewels_and_stones.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
+        unordered_map<char, int> m;
+        for(int i = 0; i < stones.size(); i++)
+            m[stones[i]]++;
+        int ans = 0;
+        for(int i = 0; i < jewels.size(); i++) {
+            if(m.find(jewels[i]) != m.end())
+                ans += m[jewels[i]];
+        }
+        return ans;
+    }
+};
+
+//another solution
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
         sort(stones.begin(), stones.end());
         sort(jewels.begin(), jewels.end());
         int i = 0, j = 0, ans = 0;
