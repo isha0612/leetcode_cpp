759bf5fd3caadf7f7915b8df4b021165e6a4e820
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-05-29 19:50:57 +0530

    Update kth_largest_element_in_an_array.cpp

--- kth_largest_element_in_an_array.cpp
+++ kth_largest_element_in_an_array.cpp
@@ -20,3 +20,50 @@ public:
         return pq.top();
     }
 };
+
+//another solution
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        vector<int> v;
+        for(int i = 0; i < nums.size(); i++) {
+            push(v, nums[i]);
+            if(v.size() > k)
+                pop(v);
+        }
+        return v[0];
+    }
+    void push(vector<int> &v, int val) {
+        v.push_back(val);
+        pushHeapify(v, v.size(), v.size() - 1);
+    }
+    void pushHeapify(vector<int> &v, int n, int i) {
+        int parent = (i - 1) / 2;
+        if(v[parent] > v[i]) {
+            swap(v[parent], v[i]);
+            pushHeapify(v, n, parent);
+        }
+    }
+    void pop(vector<int> &v) {
+        int val = v[v.size() - 1];
+        v[0] = val;
+        v.pop_back();
+        popHeapify(v, v.size(), 0);
+    }
+    void popHeapify(vector<int> &v, int n, int i) {
+        int minimum = i;
+        int l = i * 2 + 1;
+        int r = i * 2 + 2;
+        if(l < n && v[l] < v[minimum]) {
+            minimum = l; 
+        }
+        if(r < n && v[r] < v[minimum]) {
+            minimum = r; 
+        }
+        if(i != minimum) {
+            swap(v[i], v[minimum]);
+            popHeapify(v, n, minimum);
+        }
+    }
+};
