3dc059353211c954baff30502f1a9414f037ab59
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-06-02 20:08:58 +0530

    Update binary_tree_paths.cpp

--- binary_tree_paths.cpp
+++ binary_tree_paths.cpp
@@ -15,6 +15,41 @@ public:
     void inorder(TreeNode* root, string s) {
         if(!root)
             return;
+        s += to_string(root->val);
+        if(!root->left && !root->right) {
+            v.push_back(s);
+            return;
+        }
+        s += "->";
+        inorder(root->left, s);
+        inorder(root->right, s);
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        string s = "";
+        inorder(root, s);
+        return v;
+    }
+};
+
+//another solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> v;
+    void inorder(TreeNode* root, string s) {
+        if(!root)
+            return;
         s += "->";
         s += to_string(root->val);
         if(!root->left && !root->right) {
