4aab23ce3b0104ddfb36c102e12cc00f9dac210e
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-06-02 10:23:16 +0530

    Create prime_number_of_set_bits_in_binary_representation.cpp

---
+++ prime_number_of_set_bits_in_binary_representation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if(n == 1) return false;
+        for(int i = 2; i <= sqrt(n); i++) {
+            if(n % i == 0)
+                return false;
+        }
+        return true;
+    }
+    int countPrimeSetBits(int left, int right) {
+        int ans = 0;
+        for(int i = left; i <= right; i++) {
+            if(isPrime(__builtin_popcount(i)))
+                ans++;
+        }
+        return ans;
+    }
+};
